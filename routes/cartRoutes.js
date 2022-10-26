6484308d2e5db5f74b47df043fb55942c7ace3cc
up1512001 <utsav.patel1512@outlook.com>
2022-10-26 14:53:55 +0530

    still fetchcart dosen't working

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -4,116 +4,137 @@ const CartRoutes = express.Router()
 const Cart = require('../models/Cart')
 const Product = require('../models/Product')
 const User = require('../models/User')
-const {isNullorUndefinedorEmpty} = require('../utility/util')
+const { isNullorUndefinedorEmpty } = require('../utility/util')
 
-CartRoutes.post('/createcart',async(req,res)=>{
-    try{
-        if(isNullorUndefinedorEmpty(req.body.userid) && isNullorUndefinedorEmpty(req.body.productid)){
-            const getUser = await User.findOne({_id:req.body.userid})
-            const getProduct = await Product.findOne({_id:req.body.productid})
-            if(getUser !== null && getProduct !== null ){
+CartRoutes.post('/createcart', async (req, res) => {
+    try {
+        if (isNullorUndefinedorEmpty(req.body.userid) && isNullorUndefinedorEmpty(req.body.productid)) {
+            const getUser = await User.findOne({ _id: req.body.userid })
+            const getProduct = await Product.findOne({ _id: req.body.productid })
+            if (getUser !== null && getProduct !== null) {
                 const createCart = new Cart({
-                    userid:req.body.userid,
-                    productid:req.body.productid,
-                    quantity:req.body.quantity
+                    userid: req.body.userid,
+                    productid: req.body.productid,
+                    quantity: req.body.quantity
                 })
                 const saveCart = await createCart.save()
                 res.json({
-                    error:null,
-                    data:{
+                    error: null,
+                    data: {
                         ...saveCart._doc,
-                        createdAt:saveCart.createdAt.toISOString(),
-                        updatedAt:saveCart.updatedAt.toISOString()
+                        createdAt: saveCart.createdAt.toISOString(),
+                        updatedAt: saveCart.updatedAt.toISOString()
                     }
                 })
-            }else{
+            } else {
                 req.json({
-                    error:"user or product dosen't exits",
-                    data:null
+                    error: "user or product dosen't exits",
+                    data: null
                 })
             }
-        }else{
+        } else {
             res.json({
-                error:"enter required fields",
-                data:null
+                error: "enter required fields",
+                data: null
             })
         }
-    }catch(error){
+    } catch (error) {
         res.json({
-            error:"something went wrong",
-            data:null
+            error: "something went wrong",
+            data: null
         })
     }
 })
 
-CartRoutes.post('/updatecart',async(req,res)=>{
-    try{
-        if(isNullorUndefinedorEmpty(req.body.userid)){
-            const getCart = await Cart.findOne({_id:req.body.userid})
-            if(getCart !== null){
+CartRoutes.post('/updatecart', async (req, res) => {
+    try {
+        if (isNullorUndefinedorEmpty(req.body.userid)) {
+            const getCart = await Cart.findOne({ _id: req.body.userid })
+            if (getCart !== null) {
                 const updateCart = await Cart.updateOne({
-                    userid:req.body.userid
+                    userid: req.body.userid
                 },
-                {
-                    $set:{
-                        quantity:isNullorUndefinedorEmpty(req.body.quantity)?req.body.quantity:getCart.quantity
+                    {
+                        $set: {
+                            quantity: isNullorUndefinedorEmpty(req.body.quantity) ? req.body.quantity : getCart.quantity
+                        }
                     }
-                }
                 )
-                const getUpdatedCart = await Cart.findOne({_id:req.body.cartid})
+                const getUpdatedCart = await Cart.findOne({ _id: req.body.cartid })
                 res.json({
-                    error:null,
-                    data:{
+                    error: null,
+                    data: {
                         ...getUpdatedCart._doc,
-                        createdAt:getUpdatedCart.createdAt.toISOString(),
-                        updatedAt:getUpdatedCart.updatedAt.toISOString()
+                        createdAt: getUpdatedCart.createdAt.toISOString(),
+                        updatedAt: getUpdatedCart.updatedAt.toISOString()
                     }
                 })
-            }else{
+            } else {
                 res.json({
-                    error:"enter valid cartid",
-                    data:null
+                    error: "enter valid cartid",
+                    data: null
                 })
             }
-        }else{
+        } else {
             res.json({
-                error:"enter cartid",
-                data:null
+                error: "enter cartid",
+                data: null
             })
         }
-    }catch(error){
+    } catch (error) {
         res.json({
-            error:"something went wrong",
-            data:null
+            error: "something went wrong",
+            data: null
         })
     }
 })
 
-CartRoutes.post('/fetchcart',async(req,res)=>{
-    try{
-        if(isNullorUndefinedorEmpty(req.body.userid)){
+CartRoutes.post('/fetchcart', async (req, res) => {
+    try {
+        if (isNullorUndefinedorEmpty(req.body.userid)) {
             const fetchCartProduct = await Cart.aggregate([
-                {   $match:{
-                        userid:req.body.userid
+                {
+                    $match: {
+                        userid: req.body.userid
                     },
-                    $lookup:{
-                        from:"product",
-                        localField:"productid",
-                        foreignField:"_id",
-                        as:"myCustomResut"
+                    $lookup:
+                    {
+                        from: "products",
+                        let:{prodid:"$_id",active:"$isactive"},
+                        pipeline: [
+                            {
+                                $match:
+                                {
+                                    $expr:
+                                    {
+                                        $and:
+                                            [
+                                                { $eq: ["$$prodid", "$productid"] },
+                                                { $eq: ["$$active", "true"] }
+                                            ]
+                                    }
+                                }
+                            }
+                        ],
+                        as: "comments"
                     }
                 }
             ])
-        }else{
+            console.log(fetchCartProduct);
             res.json({
-                error:"enter cartid",
-                data:null
+                error:null,
+                data:fetchCartProduct
+            })
+        } else {
+            res.json({
+                error: "enter cartid",
+                data: null
             })
         }
-    }catch(error){
+    } catch (error) {
         res.json({
-            error:"something went wrong",
-            data:null
+            error: "something went wrong",
+            data: null
         })
     }
 })
